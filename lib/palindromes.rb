b4e3c63dee0bc2f7a37fe9a7bdad87a929fb4837
LDMacKrell and richa shaurbh <richa@>
2017-09-06 17:00:26 -0700

    Update app to identify palindromes of all types with WIP for extra credit.

--- lib/palindromes.rb
+++ lib/palindromes.rb
@@ -1,5 +1,8 @@
 class Palindromes
   def check_palindromes(arg)
+    if arg.class != String
+      arg = arg.to_s
+    end
     array = arg.split("")
     while array.length > 1 do
       if array.pop != array.shift
@@ -8,4 +11,9 @@ class Palindromes
     end
     true
   end
+  # def check_file(path)
+  #   file_text = File.open(path, "r+")
+  #   read_file = file_text.read()
+  #
+  # end
 end
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -10,10 +10,12 @@ describe('#check_palindrome') do
   it("return true if start and end values are the same.") do
     PossiblePalindrome = Palindromes.new()
     expect(PossiblePalindrome.check_palindromes("racecar")).to(eq(true))
+    expect(PossiblePalindrome.check_palindromes(1234321)).to(eq(true))
   end
 
   it("return false if start and end values differ.") do
     PossiblePalindrome = Palindromes.new()
     expect(PossiblePalindrome.check_palindromes("&^%7^&")).to(eq(false))
   end
+
 end
